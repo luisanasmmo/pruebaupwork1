3e834aee07db169fea2ec7b339639a35228db1b9
luisanasmmo <luisanasmmo@gmail.com>
2024-01-12 17:03:58 -0400

    Mobile layout

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Upwork</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main id="wrapper">
+        <section id="ipad_wrapper">
+            <div class="head">
+                <div class="flex-container title"><span class="title-accent">GET</span> The new</div>
+                <div class="flex-container ipad-logo">
+                    <div>iPad</div>
+                    <img class="logo" src="img/logo-pink.png" alt="iPad Air Logo">
+                </div>
+            </div>
+            <div class="flex-container ipad-display">
+                <div style="background-image: url('img/pen.png');" class="pencil"></div>
+                <div style="background-image: url('img/color-pink.png');" class="ipad">
+                    <div class="deal-tag deal-tag-top flex-container"><span>Pay Only</span> <span class="accent">3€</span></div>
+                    <div class="deal-tag deal-tag-bottom flex-container"><div>Now with</div><span class="accent">Apple Pencil 2</span></div>
+                </div>
+                <div class="flex-container colors">
+                    <div class="color-gray"></div>
+                    <div class="color-black"></div>
+                    <div class="color-blue"></div>
+                    <div class="color-green"></div>
+                    <div class="color-pink"></div>
+                </div>
+            </div>
+        </section>
+
+        <section id="form_wrapper">
+            <div>
+                <div></div>
+                <h1>Please fill in your details</h1>
+                <form action="">
+                    <div><input type="text"></div>
+                    <div><input type="text"></div>
+                    <div><input type="text"></div>
+                    <div><input type="text"></div>
+                </form>
+            
+                <div></div>
+            </div>
+            <div></div>
+        </section>
+    </main>
+    <script src="script.js"></script>
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,14 @@
+// TIME CIRCLES
+var countSeconds = document.querySelectorAll('.count');
+var countMinutes = document.querySelectorAll('.count-min');
+var count = 120;
+countSeconds.forEach(function (n) {
+    return n.innerHTML = '00';
+});
+countMinutes.forEach(function (m) {
+    return m.innerHTML = '02';
+});
+
+// Crear function secondsCounter para el contador marcha atras
+
+// Crear function changePageColor para cambiar el color
